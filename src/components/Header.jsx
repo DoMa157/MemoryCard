007baeab360b41bfaf4f4c1d8b70723531913966
DoMa157 <82661850+DoMa157@users.noreply.github.com>
2024-06-01 04:03:49 +0400

    Adjust naruto logo

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,9 +1,10 @@
 import LeaderBoard from "./Leaderboard";
+import logo from '../assets/Naruto_logo.png';
 
 export default function Header({current, highScore}){
     return (
         <div className = "header">
-            <div>Welcome to MemoryCard: <img src = 'src/assets/Naruto_logo.png'width = "100"/> Edition!</div>
+            <div>Welcome to MemoryCard: <img src = {logo} width = "100"/> Edition!</div>
             <LeaderBoard current = {current} highScore={highScore}/>
         </div>
     )
